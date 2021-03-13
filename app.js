79eff4f43fd9179d4a668ecd1496c292c545e970
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2021-03-13 01:57:51 +0100

    add bootstrap

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ app.engine('handlebars', expressHandlebars({
                 "/": lvalue / rvalue,
                 "%": lvalue % rvalue
             }[operator];
-        }}
+        },
+        //Function "Section"
+        section: function(name, options) {
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        },
+    }
 }))
 app.set('view engine', 'handlebars')
 
